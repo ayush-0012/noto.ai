82e53bbdb012d57a02fbc8811972d4232a230f5a
ayush-0012 <ayushbisht0012@gmail.com>
2024-12-21 14:40:12 +0530

    updated the notes model

--- models/notes.model.js
+++ models/notes.model.js
@@ -1,10 +1,23 @@
-import { MotionGlobalConfig } from "framer-motion";
 import mongoose from "mongoose";
 
 const notesSchema = new mongoose.Schema(
   {
+    title: {
+      type: String,
+      required: [true, "title is required"],
+      unique: false,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
     file: {
       type: String,
+      required: true,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
       required: true,
     },
   },
@@ -12,3 +25,5 @@ const notesSchema = new mongoose.Schema(
 );
 
 const Notes = mongoose.model("notes", notesSchema);
+
+export default Notes;
