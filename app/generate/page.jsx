ce0052fc05e3981afac5332809aff3e9c7a8cf2a
ayush-0012 <ayushbisht0012@gmail.com>
2024-12-28 14:49:45 +0530

    hard coded the data for testing

--- app/generate/page.jsx
+++ app/generate/page.jsx
@@ -1,45 +1,54 @@
 "use client";
 
-import { BrainCog, Chrome, MousePointer2, Sparkles } from "lucide-react";
-import { signOut } from "next-auth/react";
-import { useState } from "react";
-
-const instructions = [
-  {
-    icon: <Chrome className="w-4 h-4 text-purple-400" />,
-    title: "Navigate to Any Webpage",
-    description: "Go to the website or article you want to take notes from",
-  },
-  {
-    icon: <MousePointer2 className="w-4 h-4 text-purple-400" />,
-    title: "Click the Extension Icon",
-    description:
-      "Find and click the AI Notes Assistant icon in your browser toolbar",
-  },
-  {
-    icon: <Sparkles className="w-4 h-4 text-purple-400" />,
-    title: "Generate Notes",
-    description:
-      "Click 'Generate' and watch as AI transforms the content into well-structured notes",
-  },
-];
+import { signOut, useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+import { instructions } from "@/utils/content";
+import { BrainCog, Sparkles } from "lucide-react";
+import { useGenerateNotesUrl } from "@/hooks/useGenerateNotesUrl";
 
 const Genearate = () => {
   const [loading, setloading] = useState(false);
 
+  const { data: session } = useSession();
+
+  console.log("/generate", session);
+
+  const url =
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Inheritance_and_the_prototype_chain";
+  const fileName = "prototype chain";
+  const userId = "676fb5c319ce629dcd174766";
+
   function handleSignOut() {
     signOut({ callbackUrl: "/" });
-    localStorage.removeItem("userId");
+  }
+
+  async function handleGenerateNotes() {
+    setloading(true);
+    try {
+      await useGenerateNotesUrl(url, fileName, userId);
+    } catch (error) {
+      console.log("error occured", error);
+    } finally {
+      setloading(false);
+    }
   }
 
   return (
     <>
       <div>
-        <div className="flex items-center space-x-1 mt-4 ml-4">
-          <BrainCog className="w-6 h-6 text-purple-400" />
-          <span className="text-lg font-semibold bg-clip-text text-transparent bg-gradient-to-r from-purple-400 to-violet-400">
-            How It Works
-          </span>
+        <div className="flex items-center justify-between space-x-1 mt-4 mx-4">
+          <div className="flex gap-2">
+            <BrainCog className="w-6 h-6 text-purple-400" />
+            <span className="text-lg font-semibold bg-clip-text text-transparent bg-gradient-to-r from-purple-400 to-violet-400">
+              How It Works
+            </span>
+          </div>
+
+          <div>
+            <button onClick={() => handleSignOut()} className="text-white">
+              logout
+            </button>
+          </div>
         </div>
 
         <p className="text-2xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-purple-400 to-violet-400 flex items-center justify-center mt-12">
@@ -47,7 +56,10 @@ const Genearate = () => {
         </p>
 
         <div className="flex justify-center mt-4 w-full">
-          <button className="flex gap-2 items-center justify-center bg-gradient-to-r from-purple-500 to-violet-800 font-bold w-3/4 rounded-lg h-12">
+          <button
+            className="flex gap-2 items-center justify-center bg-gradient-to-r from-purple-500 to-violet-800 font-bold w-3/4 rounded-lg h-12"
+            onClick={() => handleGenerateNotes()}
+          >
             <Sparkles />
             {loading ? <span>Generating...</span> : <span>Generate Notes</span>}
           </button>
@@ -57,7 +69,7 @@ const Genearate = () => {
           Follow these simple steps to transform any webpage into
           well-structured notes
         </p>
-
+        {/* INSTRUCTIONS DIV */}
         <div className="p-1 mt-4">
           {instructions.map((instruction, index) => (
             <div
@@ -68,7 +80,7 @@ const Genearate = () => {
                 {instruction.icon}
               </div>
               <div className="mt-3 flex flex-col ml-10 items-start">
-                <div className="font-semibold text-base">
+                <div className="font-semibold text-base text-gray-300">
                   {instruction.title}
                 </div>
                 <div className="text-sm text-zinc-400">
