857b6916b527618e956ccbc254b2869cefaf1b66
ayush-0012 <ayushbisht0012@gmail.com>
2025-01-01 15:21:35 +0530

    minor changes

--- utils/generateNotesUrl.js
+++ utils/generateNotesUrl.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export async function useGenerateNotesUrl(url, fileName, userId) {
+export async function GenerateNotesUrl(url, fileName, userId) {
   try {
     const response = await axios.post(
       "http://localhost:3000/api/generate-notes",
@@ -11,7 +11,7 @@ export async function useGenerateNotesUrl(url, fileName, userId) {
       }
     );
 
-    console.log(response);
+    return response;
   } catch (error) {
     console.log("error generating notes", error);
   }
