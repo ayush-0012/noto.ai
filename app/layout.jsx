5b8a6793313bf146ce98cd64ab7448ee17f72720
ayush-0012 <ayushbisht0012@gmail.com>
2025-01-04 15:27:06 +0530

    wrapped all the chilren inside the context provider

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,21 +2,26 @@ import "./global.css";
 import Provider from "@/components/Provider";
 import { handler } from "./api/auth/[...nextauth]/route";
 import { getServerSession } from "next-auth";
+import NotesLinkProvider from "@/components/Context/NotesLinkProvider";
 
 export const metadata = {
   title: "Noto.ai",
   description:
     "Generate notes with one click, just go to your site from wherever you want to take/create notes and click on generate",
 };
-
+const AppProviders = ({ children, session }) => (
+  <Provider session={session}>
+    <NotesLinkProvider>{children}</NotesLinkProvider>
+  </Provider>
+);
 const RootLayout = async ({ children }) => {
   const session = await getServerSession(handler);
   return (
     <html lang="en">
       <body>
-        <Provider session={session}>
+        <AppProviders session={session}>
           <main className="app">{children}</main>
-        </Provider>
+        </AppProviders>
       </body>
     </html>
   );
