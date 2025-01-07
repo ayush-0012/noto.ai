5f5b2df2f05816cb19592e824557de629b5eea9c
ayush-0012 <ayushbisht0012@gmail.com>
2025-01-07 21:26:38 +0530

    created an api to fetch all the notes created by a user

---
+++ app/api/users/[userId]/route.js
@@ -0,0 +1,29 @@
+import User from "@/models/user.model";
+import connectDB from "@/utils/db";
+
+export async function GET(req, { params }) {
+  const userId = params.userId;
+  console.log(userId);
+  try {
+    await connectDB();
+    const user = await User.findById(userId);
+
+    if (!user) {
+      throw new Error("unable to find user", { status: 404 });
+    }
+
+    return new Response(
+      JSON.stringify(
+        {
+          user: user.generateHistory,
+        },
+        { status: 200 }
+      )
+    );
+  } catch (error) {
+    console.log("error fetching user", error);
+    return new Response(JSON.stringify({ error: error.message }), {
+      status: 500,
+    });
+  }
+}
