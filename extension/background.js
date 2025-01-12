f3418bbd074f286ca8419273233c1c9d2bd1f098
ayush-0012 <ayushbisht0012@gmail.com>
2025-01-12 14:07:14 +0530

    added a script to get the url from the actibe tab and store it in chrome storage

--- extension/background.js
+++ extension/background.js
@@ -2,3 +2,13 @@ chrome.action.onClicked.addListener(() => {
   // Open your frontend app in a new tab
   chrome.tabs.create({ url: "http://localhost:3000" });
 });
+
+chrome.action.onClicked.addListener(async () => {
+  // Get the active tab URL
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    const activeTabUrl = tabs[0].url;
+
+    // Send the URL to your modal, or store it in a variable or state
+    chrome.storage.local.set({ activeTabUrl });
+  });
+});
