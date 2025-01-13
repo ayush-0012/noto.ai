0019e760dab161c464b60f2ae0f86cd2aeb203e6
ayush-0012 <ayushbisht0012@gmail.com>
2025-01-13 14:08:33 +0530

    created an axiosInstance for efficient api calls even when the port is busy

---
+++ utils/axiosInstance.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const PORT = process.env.NEXT_PUBLIC_API_PORT || "3001";
+const API_URL = `http://localhost:${PORT}`;
+
+const axiosInstance = axios.create({
+  baseURL: API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export default axiosInstance;
