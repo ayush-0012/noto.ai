abab9d0557048a3f663e017d04faba23f632b438
ayush-0012 <ayushbisht0012@gmail.com>
2025-01-13 21:22:31 +0530

    added cors options globally

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,4 @@
 /** @type {import('next').NextConfig} */
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     domains: ["lh3.googleusercontent.com"],
@@ -11,6 +10,28 @@ const nextConfig = {
       fs: false, // Ignore 'fs' for client-side
     };
     return config;
+  },
+  async headers() {
+    return [
+      {
+        // Match all API routes
+        source: "/api/:path*", // Matches any route starting with /api/
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
   },
 };
 
