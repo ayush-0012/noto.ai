61bf6fbec62a479b6a48962063b9af34c08a625d
ayush-0012 <ayushbisht0012@gmail.com>
2025-01-15 14:52:20 +0530

    minor changes

--- next.config.mjs
+++ next.config.mjs
@@ -4,45 +4,26 @@ const nextConfig = {
     NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
   },
   output: "export",
-  distDir: "out", // Specifies the output directory
-  experimental: {
-    appDir: true, // Enable the app directory (Next.js 13+ feature)
-  },
+  distDir: ".next", // Keep default build directory
   images: {
+    unoptimized: true, // Required for static export
     domains: ["lh3.googleusercontent.com"],
-    // unoptimized: true, // Required for static export
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+    ],
   },
   assetPrefix: "./",
   basePath: "",
   webpack: (config) => {
     config.resolve.fallback = {
       ...config.resolve.fallback,
-      fs: false, // Ignore 'fs' for client-side
+      fs: false,
     };
     return config;
   },
-  // async headers() {
-  //   return [
-  //     {
-  //       // Match all API routes
-  //       source: "/api/:path*", // Matches any route starting with /api/
-  //       headers: [
-  //         {
-  //           key: "Access-Control-Allow-Origin",
-  //           value: "*",
-  //         },
-  //         {
-  //           key: "Access-Control-Allow-Methods",
-  //           value: "GET, POST, PUT, DELETE, OPTIONS",
-  //         },
-  //         {
-  //           key: "Access-Control-Allow-Headers",
-  //           value: "Content-Type, Authorization",
-  //         },
-  //       ],
-  //     },
-  //   ];
-  // },
 };
 
 export default nextConfig;
